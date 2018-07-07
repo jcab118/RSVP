a579aef8175e87187f7958487239b18df5fdece3
Joel <jcab118@yahoo.com>
2018-07-07 17:25:21 -0400

    rendering

--- src/GuestList.js
+++ src/GuestList.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 const GuestList = props => 
-<ul>
-	{props.guests.map((guests, index) =>
-	 <li key={index}>
-	    <span>{guest.name}</span>
-	    <label>
-	      <input type="checkbox" checked={guest.isConfirmed} /> Confirmed
-	    </label>
-	    <button>edit</button>
-	    <button>remove</button>
-	  </li>
-	  )
+	<ul>
+		{props.guests.map((guest, index) =>
+		 <li key={index}>
+		    <span>{guest.name}</span>
+		    <label>
+		      <input type="checkbox" checked={guest.isConfirmed} /> Confirmed
+		    </label>
+		    <button>edit</button>
+		    <button>remove</button>
+		  </li>
+	  )}
 </ul>;
 
-GuestList.proptypes = {
+GuestList.propTypes = {
 	guests: PropTypes.array.isRequired
 }
 
